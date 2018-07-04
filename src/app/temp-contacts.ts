8034994b068baad536d30a8ebc7f45ec960c0bec
Afif Ashroff <afifashroff@gmail.com>
2018-07-04 15:31:01 +0800

    Create temporary fake contacts

---
+++ src/app/temp-contacts.ts
@@ -0,0 +1,24 @@
+import { Contacts } from './contacts';
+
+export const CONTACTS: Contacts[] = [
+    { id: 1, name: 'Dukie', email: ' spinello0@unicef.org', phone: 5206837948, company: 'Funk-Harber', address: '4 Main Hill' },
+    { id: 2, name: 'Darleen', email: ' woodyear1@goo.gl', phone: 3328016730, company: 'Pollich Group', address: '709 Prairieview Lane' },
+    { id: 3, name: 'Jourdan', email: ' frankowski2@merriam-webster.com', phone: 7786365337, company: 'Homenick-Rau', address: '9406 Bultman Junction' },
+    { id: 4, name: 'Corrinne', email: ' behnecke3@eventbrite.com', phone: 5987180828, company: 'Kunde and Sons', address: '30 Steensland Circle' },
+    { id: 5, name: 'Merola', email: ' ponter4@xrea.com', phone: 4593437051, company: 'Dickinson, Pfannerstill and Keebler', address: '4751 Forster Park' },
+    { id: 6, name: 'Zaneta', email: ' mountjoy5@blogs.com', phone: 5289729886, company: 'Heidenreich and Sons', address: '50 Northport Alley' },
+    { id: 7, name: 'Maria', email: ' buntin6@fema.gov', phone: 4882956284, company: 'Murazik Inc', address: '45833 Vidon Place' },
+    { id: 8, name: 'Harvey', email: ' ragsdale7@psu.edu', phone: 7929119835, company: 'Fay-Collins', address: '598 Mesta Drive' },
+    { id: 9, name: 'Shaughn', email: ' bruyett8@mtv.com', phone: 2028645418, company: 'Hahn-Littel', address: '462 Spaight Crossing' },
+    { id: 10, name: 'Emelia', email: ' shotbolt9@vinaora.com', phone: 8556298692, company: 'Baumbach Group', address: '73 Mcbride Lane' },
+    { id: 11, name: 'Audie', email: ' antonioa@weather.com', phone: 7817239745, company: 'Murray and Sons', address: '9107 Pepper Wood Drive' },
+    { id: 12, name: 'Valeda', email: ' donnerb@deliciousdays.com', phone: 4231472830, company: 'Bosco Inc', address: '47 4th Place' },
+    { id: 13, name: 'Mirabel', email: ' dingwallc@xrea.com', phone: 4664040978, company: 'Jacobi, Conn and Kerluke', address: '2412 Division Lane' },
+    { id: 14, name: 'Colver', email: ' adessd@surveymonkey.com', phone: 8846541929, company: 'Senger and Sons', address: '0081 Canary Pass' },
+    { id: 15, name: 'Jemmy', email: ' ocarrolle@baidu.com', phone: 3096508256, company: 'Stanton-Hammes', address: '5 Dunning Way' },
+    { id: 16, name: 'Justinian', email: ' haythornthwaitef@mapy.cz', phone: 6149101157, company: 'Paucek, Maggio and Gerlach', address: '2805 Mayer Point' },
+    { id: 17, name: 'Ernest', email: ' wilkinsg@merriam-webster.com', phone: 1807335229, company: 'Hermann Group', address: '05039 Waxwing Parkway' },
+    { id: 18, name: 'Laurence', email: ' glisonh@salon.com', phone: 5999996919, company: 'Friesen-Wuckert', address: '8 Bay Crossing' },
+    { id: 19, name: 'Blinni', email: ' mcbreartyi@jigsy.com', phone: 3132408358, company: 'Stracke LLC', address: '601 Harbort Plaza' },
+    { id: 20, name: 'Eloisa', email: ' chassonj@a8.net', phone: 3623214514, company: 'Kuhn, Carroll and Murazik', address: '0385 Spohn Pass' }
+];
